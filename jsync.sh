2ef68f5a1608a2b4de9b3a3eceb5bcaa110428de
jeromecst <jerome.coqui@gmail.com>
2020-05-31 18:47:38 +0200

    making it less user specific

--- jsync.sh
+++ jsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -arv --progress --delete -e ssh jserv@homelocal:/home/jserv/ /home/jerome/.jserv/ --files-from=/home/jerome/.config/jsync-rsync --exclude ".*/"
-rsync -arv --progress --delete -e ssh jserv@home:/home/jserv/ /home/jerome/.jserv/ --files-from=/home/jerome/.config/jsync-rsync --exclude ".*/"
+rsync -arv --progress --delete -e ssh jserv@homelocal:/home/jserv/ /home/$USER/.jserv/ --files-from=/home/$USER/.config/jsync-rsync --exclude ".*/"
+rsync -arv --progress --delete -e ssh jserv@home:/home/jserv/ /home/$USER/.jserv/ --files-from=/home/$USER/.config/jsync-rsync --exclude ".*/"
