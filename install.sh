790409de413308d8b7feb78c35f41550d0022f54
jeromecst <jerome.coquisart@u-psud.fr>
2020-09-20 09:46:07 +0100

    correcting chown command

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 sudo rsync -urv --progress * /usr/local/bin/
 sudo rm /usr/local/bin/install.sh
-sudo chown $USER /usr/local/bin 
-chmod 755 /usr/local/bin
+sudo chown -R $USER /usr/local/bin 
+chmod 755 -R /usr/local/bin
