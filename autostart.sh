4a98b82d9823e994552e15e5d6f13745e259fadf
jerome <jexowa@gmail.com>
2020-12-23 12:21:12 +0100

    don't need feh anymore

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 bar.sh&
 setxkbmap -option caps:escape
 redshift&
-feh --no-fehbg --bg-scale $HOME/.wallpaper& 
+hsetroot -fill $HOME/.wallpaper& 
 if [ ! -f "/sys/class/power_supply/BAT0/status" ]
 then 
 	picom --config $HOME/.config/picom.conf& 
