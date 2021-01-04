4b565d16c138b5c6b7bb759774e22d5a5be871df
jerome <jexowa@gmail.com>
2021-01-04 02:52:18 +0100

    autostart fix

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,7 @@ redshift&
 hsetroot -fill $HOME/.wallpaper& 
 if [ -f "/sys/class/power_supply/BAT0/status" ]
 then
-	grep size $HOME/.config/alacritty_default.yml | sed -e "s/[0-9]\+/10/" > $HOME/.config/alacritty/alacritty.yml
+	sed -e "s/size: [0-9]\+/size: 9/" $HOME/.config/alacritty_default.yml  > $HOME/.config/alacritty/alacritty.yml
 	light -N 1
 else
 	cp $HOME/.config/alacritty_default.yml $HOME/.config/alacritty/alacritty.yml
